17b6ad46b5b6789d05ba4a00670fed60334d460e
sonalpkhare1 <sonalpkhare1@gmail.com>
2023-05-23 07:05:50 +0400

    Changed

--- src/basicConcepts/StringPrac.java
+++ src/basicConcepts/StringPrac.java
@@ -2,7 +2,9 @@ package basicConcepts;
 
 public class StringPrac {
 	public static void main(String[]args) {
-		System.out.println("I am in main method");
+		System.out.println("I am in main method.....of branch A");
 	}
-
+	 void a() {
+		 System.out.println("stash5");
+	 }
 }
