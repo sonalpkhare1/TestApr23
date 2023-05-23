760d582b8e619602170a5f0b4cd41f0a43f9eea0
sonalpkhare1 <sonalpkhare1@gmail.com>
2023-05-23 10:15:45 +0400

    Changes in StringPrac file

--- src/basicConcepts/StringPrac.java
+++ src/basicConcepts/StringPrac.java
@@ -3,8 +3,6 @@ package basicConcepts;
 public class StringPrac {
 	public static void main(String[]args) {
 		System.out.println("I am in main method.....of branch A");
-	}
-	 void a() {
-		 System.out.println("stash5");
+		System.out.println("Creating Conflict scenario from A and B branch");
 	 }
 }
